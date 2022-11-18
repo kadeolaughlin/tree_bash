01a02d1915dc912dd1fa1a2bf3af9d762ca82588
kadeolaughlin <55366417+kadeolaughlin@users.noreply.github.com>
2022-11-18 14:41:10 -0600

    Update tree.sh

--- tree.sh
+++ tree.sh
@@ -1,62 +1,32 @@
 #!/bin/bash
-##Based on https://blog.niklasottosson.com/linux/bash-script-simulating-the-tree-command/
+
 olddir=$PWD;
 
 declare -i dirdepth=0;
-printf ".: $(find $file -type f | wc -l) total files\n";
+printf ".: $(find $file  -maxdepth 1 -type f | wc -l) total files\n";
+#printf ".: $(find $file -maxdepth 1 -not -path '*/[@.]*' -type f | wc -l) total files\n";
 function getfilecount {
-    cd "$1";
+    cd -- "$1";
 
     for file in $(ls -a)
     do
-        if [ -d $file ] && [ $file != "$1" ] && [ $file != "$2" ]
+        if [ -d $file ] && [ $file != "." ] && [ $file != ".." ]
         then
             for ((i=0; $i < $dirdepth; i++))
             do
                 printf "|   "
             done
 
-            printf "|-- $file: $(find $file -type f | wc -l)\n"
+            printf "|__ $file: $(find $file  -maxdepth 1 -type f | wc -l)\n"
+            #printf "|-- $file: $(find $file -maxdepth 1 -not -path '*/[@.]*' -type f | wc -l)\n"
             let dirdepth=$dirdepth+1;
-            getfilecount "$file" "$file/..";
+            getfilecount "$file";
             cd ..;
         fi
     done
     let dirdepth=$dirdepth-1;
 }
-getfilecount "$1" "$1/..";
+getfilecount "$1";
 
 cd $olddir;
 unset i dirdepth;
-
-#printf "\e[1;34m.\e[0m\n";
-#function listfiles {
-#    cd "$1";
-#    for file in *
-#    do
-#        for ((i=0; $i < $dirdepth; i++))
-#        do
-#            ##Tab between each level
-#            printf "│   ";
-#        done
-#        ##Print directories with brackets ([directory])
-#        if [ -d "$file" ]
-#        then
-#            printf "├── \e[1;34m$file\e[0m\n";
-#            alldirs=$alldirs+1;
-#        else
-#            printf "├── $file\e[0m\n";
-#            allfiles=$allfiles+1;
-#        fi
-#
-#        ##Work our way through the system recursively
-#        if [ -d "$file" ]
-#        then
-#            dirdepth=$dirdepth+1;
-#            listfiles "$file";
-#            cd ..;
-#        fi
-#    done
-#    ##Done with this directory - moving on to next file
-#    let dirdepth=$dirdepth-1;
-#}
